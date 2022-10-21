44511f5b40733526fc5ff1c4d2f865cb7b816b88
Carmela Regife <c.regife.sanchez@gmail.com>
2022-10-21 11:31:33 +0200

    second commit

--- src/com/pruebaCSV/PruebaCSV.java
+++ src/com/pruebaCSV/PruebaCSV.java
@@ -2,10 +2,7 @@ package com.pruebaCSV;
 
 public class PruebaCSV {
     public static void main(String[] args) {
-        String csv = "1,2,3,4,5,6,7,8,9,10";
-        String[] values = csv.split(",");
-        for (String value : values) {
-            System.out.println(value);
-        }
+       LectorCSV lector = new LectorCSV();
+            lector.leeCSV("./datos/pokemon.csv");
     }
 }
